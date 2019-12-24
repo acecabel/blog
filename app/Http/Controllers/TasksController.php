e3576f71da001e6ae82af425e351b944932ccdcb
ite19-intl-33@s312.uc-bcf.edu.ph <ite19-intl-33@s312.uc-bcf.edu.ph>
2019-12-24 16:13:29 +1200

    First commit

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    //
+    public function index()
+    {
+    	return view('tasks.index');
+    }
+     
+     public function create()
+    {
+    	return view('tasks.create');
+    }
+
+    public function update()
+    {
+    	return view('tasks.update');
+    }
+}
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@section('content')
+
+@include('layouts.errors')
+This is the index file
+
+@endsection
+
+@section('other_footer')
+This is the other Footer
+@endsection
+
+
